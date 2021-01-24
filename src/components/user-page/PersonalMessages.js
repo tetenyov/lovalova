e6fe5ad4e593ab7bd62888333ebd93bffdc66b0c
Vovo <tetenev@yandex.ru>
2021-01-24 15:12:33 +0300

    added formik form in PersonalMessages component

--- src/components/user-page/PersonalMessages.js
+++ src/components/user-page/PersonalMessages.js
@@ -1,78 +1,53 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
-import ButtonSend from '../../components/common/ButtonSend'
+import ButtonSend from '../common/ButtonSend'
+import { getMessagesList } from '../../utils/utils'
 
 import './styles/form-pm.scss'
 
-class PersonalMessages extends Component {
-  state = {
-    message: '',
-    messages: [],
-    isOpen: false
-  }
+function PersonalMessages() {
 
-  inputChangeHandler = (evt) => {
-     this.setState({
-      message: evt.target.value
-    })
-  }
-
-  formSubmitHandler = (evt) => {
-    evt.preventDefault()
-    this.setState(state => ({
-      message: '',
-      messages: [...state.messages, this.state.message]
-    }))
-  }
-
-  labelClickHandler = () => {
-     this.setState(state => ({
-       isOpen: !state.isOpen
-     }))
-  }
-
-  render() {
-    const { messages } = this.state
-
-    const getMessagesList = () => {
-      return (
-        messages.map((message, i) => {
-          return (
-            <li className='form-pm__message' key={i}>{ message }</li> 
-          )
-        })
-      )
-    }
-
-    const messagesList = messages.length 
-      ? getMessagesList()
-      : <p className='form-pm__notice'>no messages yet</p>;
-
-    return (
-      <form onSubmit={ this.formSubmitHandler } className='form-pm'>
-        <label className='form-pm__label' htmlFor='pm-textarea'
-          onClick={ this.labelClickHandler }
-        >
-          Say something
-        </label>
-    
-        { 
-          this.state.isOpen 
-          && <section className='form-pm__dialog'>
-              <ul className='form-pm__messages-list'>
-                { messagesList }
-              </ul>
-              <textarea className='form-pm__message-textarea' id='pm-textarea'
-                placeholder='type here'
-                value={ this.state.message }
-                onChange={ this.inputChangeHandler }
-              />
-              <ButtonSend />
-            </section>
-        }
-      </form>
-    )
-  }
+  return (
+    <Formik
+      initialValues={{
+        message: '',
+        messages: [],
+        isOpen: false
+      }}
+      onSubmit={(values, {setFieldValue}) => {
+        setFieldValue('messages',  [...values.messages, values.message])
+        setFieldValue('message',  '')
+      }}
+    >
+      {({values, handleChange, setFieldValue}) => (
+        <Form>
+          <label className='form-pm__label' htmlFor='pm-textarea'
+            onClick={() => setFieldValue('isOpen', !values.isOpen)}
+          >
+            Say something
+          </label>
+          { 
+            values.isOpen &&
+              <section className='form-pm__dialog'>
+                <ul className='form-pm__messages-list'>
+                  { getMessagesList(values.messages) }
+                </ul>
+                <Field as='textarea' className='form-pm__message-textarea' id='pm-textarea'
+                  placeholder='type here'
+                  value={values.message}
+                  onChange={(evt) => {
+                    handleChange(evt)
+                    setFieldValue('message', evt.target.value)
+                  }}
+                />
+                <ButtonSend />
+              </section>
+          }
+        </Form>
+      )}
+    </Formik>
+  )
 }
 
 export default PersonalMessages
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export function validateEditProfile(inputData) {
 
   return Object.values(inputData).every(input => input.length !== 0)
@@ -5,4 +7,14 @@ export function validateEditProfile(inputData) {
 
 export function isChecked(checked, name) {
   return checked.includes(name)
+}
+
+export function getMessagesList(messages) {
+  return (
+    messages.map((message, i) => {
+      return (
+        <li className='form-pm__message' key={i}>{ message }</li> 
+      )
+    })
+  )
 }
