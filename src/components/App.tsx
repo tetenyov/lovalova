8152f80023893d14d2bb3fc2aa32cdcce5507d41
vovo <tetenev@yandex.ru>
2021-07-19 12:29:16 +0300

    rewrite Header component and moved RootState type from store file to own

---
+++ src/components/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom'; 
+
+import Header from './header/Header';
+import EditProfile from './edit-profile/EditProfile';
+import MainPage from './main-page/MainPage';
+import UserPage from './user-page/UserPage';
+
+import '../styles-global/site-wrapper.scss';
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  return (
+    <div className={pathname === '/' ? 'site-wrapper' : 'site-wrapper site-wrapper--inner'}>
+      <Switch>
+        <Route exact path='/' component={Header}/>
+        <Route path={['/female', '/male']} component={MainPage}/>
+        <Route path='/user/:id' component={UserPage}/>
+        <Route path='/edit' component={EditProfile}/>
+      </Switch>
+    </div>
+  );
+}
